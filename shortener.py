11da43dab1e929a2a45f83f3e8dbf6e519a79886
Aryanwadhonkar <161907825+Aryanwadhonkar@users.noreply.github.com>
2025-03-31 06:05:15 +0530

    Create shortener.py

---
+++ shortener.py
@@ -0,0 +1,19 @@
+import requests
+from config import Config
+
+def shorten_url(url: str) -> str:
+    if not Config.URL_SHORTENER_API or not Config.URL_SHORTENER_DOMAIN:
+        return url
+    
+    try:
+        response = requests.post(
+            f"https://{Config.URL_SHORTENER_DOMAIN}/api",
+            json={
+                "api_key": Config.URL_SHORTENER_API,
+                "url": url
+            },
+            timeout=5
+        )
+        return response.json().get("shortened_url", url)
+    except Exception:
+        return url
