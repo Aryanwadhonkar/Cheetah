106ed98ac5e305e96f3c922352c204049df2faeb
Aryanwadhonkar <161907825+Aryanwadhonkar@users.noreply.github.com>
2025-03-31 06:09:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
-    ADMINS = os.getenv("ADMINS").split(",")
+    ADMINS = [int(admin) for admin in os.getenv("ADMINS").split(",")]
     DB_CHANNEL = int(os.getenv("DB_CHANNEL"))
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
-    FORCE_SUB = os.getenv("FORCE_SUB")
-    AUTO_DELETE = int(os.getenv("AUTO_DELETE", 0))
+    FORCE_SUB = os.getenv("FORCE_SUB", "0")
+    AUTO_DELETE = int(os.getenv("AUTO_DELETE", "0"))
+    URL_SHORTENER_API = os.getenv("URL_SHORTENER_API", "")
+    URL_SHORTENER_DOMAIN = os.getenv("URL_SHORTENER_DOMAIN", "")
